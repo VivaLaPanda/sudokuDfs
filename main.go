fb63543ad3ec0255d8c603721d59bebdb4f4e208
Adrian Smith <Adrian Smith>
2017-02-12 22:15:26 -0800

    Added simple command main

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
 
+	dfs "github.com/VivaLaPanda/sudokuDfs/depthfirstsearch"
+)
+
+func main() {
+	fmt.Println("Please enter the sudoku text file (e.g. file.txt): ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	fmt.Println("Thinking... ")
+	board := dfs.SolveBoard(scanner.Text())
+
+	if board != nil {
+		fmt.Println("Solution Found!")
+		fmt.Println("Please enter the file to save your solution in (e.g. file.txt): ")
+		scanner.Scan()
+		board.Dump(scanner.Text())
+	}
 }
